f9197f2aa232c1ff0c2e114473ee4ba79ec505d4
Szczepan <w67131@student.wsiz.edu.pl>
2023-05-11 16:25:27 +0200

    zmiana nazwa, dodałem funkcję pomocniczą

--- Lab1/1b_3.py
+++ Lab1/1b_3.py
@@ -1,17 +1,22 @@
-tab = []
+def int_to_str(liczba):
+    return str(liczba)
 
 
-n = int(input("Podaj ilosc elementów: "))
+lista_elementow = []
 
-for i in range(n): 
-    tab.append(str(input(f"podaj {i+1} element: ")))
+ilosc_elementow = int(input("Podaj ilość elementów: "))
 
+for i in range(ilosc_elementow):
+    element = int(input(f"Podaj {i+1}. element: "))
+    lista_elementow.append(element)
 
-for i in range(len(tab) - 1):
-    min = i
-    for j in range(i + 1, len(tab)):
-        if tab[j] < tab[min]:
-            min = j
-    zamien = tab[i]
-    tab[i] = tab[min]
-    tab[min] = zamien
+for i in range(len(lista_elementow) - 1):
+    indeks_minimalnego = i
+    for j in range(i + 1, len(lista_elementow)):
+        if int_to_str(lista_elementow[j]) < int_to_str(lista_elementow[indeks_minimalnego]):
+            indeks_minimalnego = j
+    zamien = lista_elementow[i]
+    lista_elementow[i] = lista_elementow[indeks_minimalnego]
+    lista_elementow[indeks_minimalnego] = zamien
+
+print(lista_elementow)
