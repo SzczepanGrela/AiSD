2e7106b21548d035f69f3a481c77c7162ca69908
Szczepan <w67131@student.wsiz.edu.pl>
2023-05-11 13:23:51 +0200

    Zadania 4-5

---
+++ Lab1/1a_4.py
@@ -0,0 +1,11 @@
+import random
+
+tab = [random.randint(0, 100) for _ in range(10)]
+i = 1
+min = tab[0]
+
+while i < len(tab):
+    if tab[i] < min:
+        min = tab[i]
+    i += 1
+print(f"najmniejszą liczbą jest {min}")
