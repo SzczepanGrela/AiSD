05cbfb538803e3dc3a6b65aeb959401b279dba05
Szczepan <w67131@student.wsiz.edu.pl>
2023-05-11 18:53:04 +0200

    przeniesienia printa do funkcji

--- Lab1/1r_1_wer_II_reku.py
+++ Lab1/1r_1_wer_II_reku.py
@@ -1,9 +1,9 @@
 def NWD_II_reku(a,b):
     
-    if b==0: return a
+    if b==0: return print(f"NWD wynosi: {a}")
     else: return NWD_II_reku(b,a%b)
          
 a=int(input("Podaj a: "))
 b=int(input("Podaj b: "))
 
-print(f"NWD wynosi: {NWD_II_reku(a,b)}")
+NWD_II_reku(a,b)
