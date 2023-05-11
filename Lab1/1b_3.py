32e93cdc59a7246d0103db92ec6ee4884890a6c9
Szczepan <w67131@student.wsiz.edu.pl>
2023-05-11 16:34:03 +0200

    powrót do starych nazw/ mniej miejsca kod zajmuje i wydaje się być bardziej czytelne

--- Lab1/1b_3.py
+++ Lab1/1b_3.py
@@ -1,22 +1,23 @@
-def int_to_str(liczba):
-    return str(liczba)
+"""Funkcja pomocnicza zwracająca rozwinięcie dziesiętne liczby jako łańcuch znaków."""
+def int_to_str(a):    
+     return str(a)
+
+tab = []
 
 
-lista_elementow = []
+n = int(input("Podaj ilosc elementów: "))
 
-ilosc_elementow = int(input("Podaj ilość elementów: "))
+for i in range(n): 
+    tab.append(int(input(f"podaj {i+1} element: ")))
 
-for i in range(ilosc_elementow):
-    element = int(input(f"Podaj {i+1}. element: "))
-    lista_elementow.append(element)
 
-for i in range(len(lista_elementow) - 1):
-    indeks_minimalnego = i
-    for j in range(i + 1, len(lista_elementow)):
-        if int_to_str(lista_elementow[j]) < int_to_str(lista_elementow[indeks_minimalnego]):
-            indeks_minimalnego = j
-    zamien = lista_elementow[i]
-    lista_elementow[i] = lista_elementow[indeks_minimalnego]
-    lista_elementow[indeks_minimalnego] = zamien
+for i in range(len(tab) - 1):
+    min = i
+    for j in range(i + 1, len(tab)):
+        if int_to_str(tab[j]) < int_to_str(tab[min]):
+            min = j
+    zamien = tab[i]
+    tab[i] = tab[min]
+    tab[min] = zamien
 
-print(lista_elementow)
+print(tab)
